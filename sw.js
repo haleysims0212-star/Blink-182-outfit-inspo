f3e25b0b83e640b83441054cf772e87e6c9b0109
haleysims0212-star <haley.sims0212@gmail.com>
2026-09-17 18:15:46 -0500

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='inspo-projects-v1';
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+});
